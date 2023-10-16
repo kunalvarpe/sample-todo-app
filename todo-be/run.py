a67d61b48a1d9df7e2f8f3f3c5a464c536fe75b0
sumeet-baghel <sbaghel@teikametrics.com>
2023-10-16 18:00:05 +0530

    change default port to 5000

--- todo-be/run.py
+++ todo-be/run.py
@@ -1,6 +1,6 @@
 from app import app
 import os
 
-# If file is called directly called, then run the app in debug mode on the PORT provided defined in ENV or use '3000'.
+# If file is called directly called, then run the app in debug mode on the PORT provided defined in ENV or use '5000'.
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=os.getenv('PORT', 3000), debug=True)
+    app.run("0.0.0.0", port=os.getenv('PORT', 5000), debug=True)
