c4d45aadde09bc38ff7abc4ed6f91004c86d3a36
Sumeet <sbaghel@teikametrics.com>
2023-10-29 22:50:54 +0530

    Fix Frontend, Backend, and add docker-compose with Usage instructions (#6)

--- todo-be/modules/hello.py
+++ todo-be/modules/hello.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import jsonify
+from flask_cors import cross_origin
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def hello():
   return jsonify({"message":"Hello from Flask!"})
--- todo-be/modules/todo.py
+++ todo-be/modules/todo.py
@@ -1,15 +1,17 @@
 import requests
 from app import app
 from utils.logger.logger import setup_console_root_logger
+from flask_cors import cross_origin
 
 logger = setup_console_root_logger()
 
-URL = "https://jsonplaceholder.typicode.com/todos"
+TODO_API_URL = "https://jsonplaceholder.typicode.com/todos"
 
 @app.route('/todo')
+@cross_origin(supports_credentials=True)
 def todo_all():
     try:
-        res = requests.get(url = URL).json()
+        res = requests.get(url = TODO_API_URL).json()
         logger.debug(res)
         return res
     except Exception as e:
@@ -17,9 +19,10 @@ def todo_all():
         exit(1)
 
 @app.route('/todo/<int:id>')
+@cross_origin(supports_credentials=True)
 def todo_id(id):
     try:
-        res = requests.get(url = f"{URL}/{id}").json()
+        res = requests.get(url = f"{TODO_API_URL}/{id}").json()
         logger.debug(res)
         return res
     except Exception as e:
